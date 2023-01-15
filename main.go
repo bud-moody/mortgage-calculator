1f6c0c3ff4b848979dbda3a5a66ab7e2a706fc21
budmoody <buddymoody@gmail.com>
2023-01-15 13:51:44 +0000

    Without using Go Kit..

--- main.go
+++ main.go
@@ -1,56 +1,39 @@
 package main
 
 import (
-	"context"
+
 	"encoding/json"
+
 	"log"
 	"net/http"
-	"github.com/go-kit/kit/endpoint"
-	httptransport "github.com/go-kit/kit/transport/http"
 )
 
 func main() {
-	helloWorldService := helloWorldService{}
-
-	httpHandler := httptransport.NewServer(
-		makeHelloWorldEndpoint(helloWorldService),
-		decodeRequest,
-		encodeResponse,)
-
-	http.Handle("/helloWorld", httpHandler)
+	http.HandleFunc("/helloWorld", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
 
-type helloWorldRequest struct {
-	S string `json:"s"`
+func handler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(response(helloWorldService{}))
 }
+
 
 type helloWorldResponse struct {
 	V   string `json:"v"`
 	Err string `json:"err,omitempty"`
 }
 
-
-func decodeRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	var request helloWorldRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		return nil, err
-	}
-	return request, nil
-}
-
-func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
-	return json.NewEncoder(w).Encode(response)
-}
-
-func makeHelloWorldEndpoint(svc HelloWorldService) endpoint.Endpoint {
-	return func(_ context.Context, request interface{}) (interface{}, error) {
+func response(svc HelloWorldService) any {
+	
 		
 		v, err := svc.HelloWorld()
 		if err != nil {
-			return helloWorldResponse{v, err.Error()}, nil
+			return helloWorldResponse{v, err.Error()}
 		}
-		return helloWorldResponse{v, ""}, nil
-	}
+
+		return helloWorldResponse{v, ""}
+	
 }
