4c33dfcddc6fef5021ca65031d1540093f443c8d
budmoody <buddymoody@gmail.com>
2023-01-26 16:35:19 -0500

    add simple unit test

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "testing"
+)
+
+func TestFindMonthlyPaymentIteratively(t *testing.T) {
+	cr := CalculationRequest{}
+	cr.DurationInYears = 20
+	cr.InterestRate = 0.01
+	cr.LoanAmount = 500_000_00
+
+	actual := findMonthlyPaymentIteratively(cr)
+	if (actual != 2_290_60) {
+		t.Fatalf("Expected: %d but got %d", 2_290_60, actual)
+	}
+}
